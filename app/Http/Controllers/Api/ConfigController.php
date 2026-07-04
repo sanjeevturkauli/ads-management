c3ea0cab41d751d945a1c2362919bf9df4b77bcd
Sanjeev Kumar <radhe1310395@gmail.com>
2026-07-04 14:38:02 +0530

    fix api issue

--- app/Http/Controllers/Api/ConfigController.php
+++ app/Http/Controllers/Api/ConfigController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Services\ApplicationService;
 use App\Services\ConfigService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -10,7 +11,8 @@ use Illuminate\Http\Request;
 class ConfigController extends Controller
 {
     public function __construct(
-        private readonly ConfigService $configService
+        private readonly ConfigService $configService,
+        private readonly ApplicationService $applicationService
     ) {
     }
 
